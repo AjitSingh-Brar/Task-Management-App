2d7d63ff346574a2e31b8bd366cb0ca2a9d4fb35
NS918326 <Navrajgaltee@outlook.com>
2024-08-26 22:19:27 -0400

    ADD: Implemented localStorage to manage the Tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -30,6 +30,14 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if(tasks) {
+        this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -42,9 +50,11 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
