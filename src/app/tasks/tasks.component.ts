c01893f5630b7c2ee7c4aee86dafbecbb0721a35
NS918326 <Navrajgaltee@outlook.com>
2024-09-19 16:43:50 -0400

    UPDT: Migration of standalone components to Module-based application.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
-import { type NewTaskData } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 // interface User {
@@ -11,8 +8,6 @@ import { TasksService } from './tasks.service';
 // }
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
